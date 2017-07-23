59ce69fec6a47006c221adbf18ab7002108fdc67
Ahmed Hamdy Mahmoud <ahmedhamdyau@gmail.com>
2017-07-23 20:37:31 +0200

    Packet Stream Parsing (#7)
    * rename PacketHeader to OFPacketHeader
    
    * change variable names, use optional for parsing
    
    * packet stream parsing

---
+++ src/main/of_packets/OFPacket.java
@@ -0,0 +1,34 @@
+package of_packets;
+
+import org.jetbrains.annotations.NotNull;
+
+public class OFPacket {
+    OFPacketHeader header;
+    byte[] data;
+
+    public OFPacket(@NotNull OFPacketHeader header, @NotNull byte[] data) {
+
+        if (header.isInvalid()) {
+            throw new IllegalArgumentException("Invalid OF Header");
+        }
+
+        this.header = header;
+        this.data = data;   // Can be any OF message
+    }
+
+    public boolean isHeaderOnly() {
+        return this.data.length == 0;
+    }
+
+    public String getPakcetType() {
+        return this.header.getMessageType();
+    }
+
+    public OFPacketHeader getHeader() {
+        return header;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+}
--- src/main/of_packets/OFPacketHeader.java
+++ src/main/of_packets/OFPacketHeader.java
@@ -1,68 +1,79 @@
 package of_packets;
 
-import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteStreams;
 import org.jetbrains.annotations.NotNull;
 
-import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.util.Optional;
 
 /**
  * Represents an OF packet header
  */
-public class PacketHeader {
+public class OFPacketHeader {
     private static final HashMap<Integer, String> MSG_TYPE;
-    private static final int HEADER_LEN = 8;
+    public static final int HEADER_LEN = 8;
     private byte version;
-    private String message_type;
-    private short len;
-    private int x_id;
+    private String messageType;
+    private int len;
+    private int xId;
     private boolean valid;
 
-    private PacketHeader(byte version, byte msg_t_id, short len, int x_id) {
+    private OFPacketHeader(byte version, byte msg_t_id, int len, int x_id) {
+
+        // FIXME some TCP packets may match the OFMSG type. we need to find
+        // a better checking mechanism as raw TCP packets are used for testing so far
 
         this.version = version;
         this.len = len;
-        this.x_id = x_id;
+        this.xId = x_id;
         if (MSG_TYPE.containsKey((int) msg_t_id)) {
-            this.message_type = MSG_TYPE.get((int) msg_t_id);
+            this.messageType = MSG_TYPE.get((int) msg_t_id);
             this.valid = true;
         } else {
             this.valid = false;
         }
     }
 
-    private PacketHeader() {
+    private OFPacketHeader() {
     }
 
-    private static PacketHeader CreateInvalid() {
-        PacketHeader header = new PacketHeader();
+    private static OFPacketHeader CreateInvalid() {
+        OFPacketHeader header = new OFPacketHeader();
         header.valid = false;
         return header;
     }
 
-    @NotNull
-    public static PacketHeader ParsePacket(ByteArrayDataOutput dataOut) {
-        byte[] bytes = dataOut.toByteArray();
+    /**
+     * Extracts the first OF packet header from an array of bytes containing
+     * one or more packet headers.
+     *
+     * @param bytes Byte array containing one or more packet headers
+     * @return - Parsed {@link OFPacketHeader} if the input condition is met
+     * - Invalid {@link OFPacketHeader} If the byte array provided is smaller than OF Header
+     */
 
-        if (bytes.length < PacketHeader.HEADER_LEN) {
-            return PacketHeader.CreateInvalid();
+    @NotNull
+    public static Optional<OFPacketHeader> ParseHeader(byte[] bytes) {
+        if (bytes.length < OFPacketHeader.HEADER_LEN) {
+            return Optional.empty();
         }
 
-        ByteBuffer buff = ByteBuffer.wrap(bytes);
+        ByteArrayDataInput buff = ByteStreams.newDataInput(bytes);
 
-        byte version = buff.get();
-        byte msg_t = buff.get();
-        short len = buff.getShort();
-        int x_id = buff.getInt();
+        byte version = buff.readByte();
+        byte msg_t = buff.readByte();
+        int len = buff.readUnsignedShort();
+        int x_id = buff.readInt();
 
-        return new PacketHeader(version, msg_t, len, x_id);
+        return Optional.of(new OFPacketHeader(version, msg_t, len, x_id));
     }
 
     @Override
     public String toString() {
         return String.format(
                 "VER:%d TYPE:%s LEN:%d T_ID:%d%n",
-                this.version, this.message_type, this.len, this.x_id);
+                this.version, this.messageType, this.len, this.xId);
     }
 
     public boolean isValid() {
@@ -97,12 +108,12 @@ public class PacketHeader {
         MSG_TYPE.put(19, "Barrier Reply");
     }
 
-    public short getLen() {
+    public int getLen() {
         return len;
     }
 
-    public String getMessage_type() {
-        return message_type;
+    public String getMessageType() {
+        return messageType;
     }
 
 }
