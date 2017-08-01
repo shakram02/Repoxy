d975d31cb09edf4895560cee48f840147e758588
shakram02 <ahmedhamdyau@gmail.com>
2017-08-01 20:17:10 +0200

    add new log levels

--- src/main/utils/logging/ColoredConsoleFormatter.java
+++ src/main/utils/logging/ColoredConsoleFormatter.java
@@ -1,6 +1,7 @@
 package utils.logging;
 
 import java.util.logging.Formatter;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 public class ColoredConsoleFormatter extends Formatter {
@@ -9,9 +10,21 @@ public class ColoredConsoleFormatter extends Formatter {
     @Override
     public String format(LogRecord logRecord) {
 
-        return ConsoleColors.RED_BRIGHT + "XX" + logRecord.getMessage() + " " + logRecord.getSourceMethodName()
+        return this.levelColor(logRecord.getLevel()) + "XX" + logRecord.getMessage() + " " + logRecord.getSourceMethodName()
                 + " \n" + ConsoleColors.RESET;
     }
 
+    private String levelColor(Level level) {
+        if (level == NetworkLogLevels.CONTROLLER) {
+            return ConsoleColors.YELLOW_BOLD;
+        } else if (level == NetworkLogLevels.REPLICA) {
+            return ConsoleColors.GREEN;
+        } else if (level == NetworkLogLevels.SWITCH) {
+            return ConsoleColors.CYAN_BOLD;
+        } else if (level == NetworkLogLevels.DIFFER) {
+            return ConsoleColors.RED_UNDERLINED;
+        }
+        return ConsoleColors.WHITE;
+    }
 
 }
