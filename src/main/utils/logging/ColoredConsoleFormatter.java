0be02a18bca7d48f07e3d688dcddadebf234abd3
shakram02 <ahmedhamdyau@gmail.com>
2017-08-01 20:00:48 +0200

    custom logger with colors

--- src/main/utils/logging/ColoredConsoleFormatter.java
+++ src/main/utils/logging/ColoredConsoleFormatter.java
@@ -3,10 +3,15 @@ package utils.logging;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
-public class ConsoleFormatter extends Formatter {
+public class ColoredConsoleFormatter extends Formatter {
+
+
     @Override
     public String format(LogRecord logRecord) {
 
-        return "XX" + formatMessage(logRecord);
+        return ConsoleColors.RED_BRIGHT + "XX" + logRecord.getMessage() + " " + logRecord.getSourceMethodName()
+                + " \n" + ConsoleColors.RESET;
     }
+
+
 }
--- src/main/utils/logging/NetworkLogLevels.java
+++ src/main/utils/logging/NetworkLogLevels.java
@@ -1,4 +1,13 @@
 package utils.logging;
 
-public class NetworkLogLevels {
+import java.util.logging.Level;
+
+public class NetworkLogLevels extends Level {
+    public static final Level CONTROLLER = new NetworkLogLevels("CONTROLLER", 801);
+    public static final Level SWITCH = new NetworkLogLevels("SWITCH", 802);
+    public static final Level REPLICA = new NetworkLogLevels("REPLICA", 803);
+
+    protected NetworkLogLevels(String name, int value) {
+        super(name, value);
+    }
 }
