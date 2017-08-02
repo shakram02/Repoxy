271f15ddc590e74cd418fc2d12dbb68d48994cb9
shakram02 <ahmedhamdyau@gmail.com>
2017-08-02 08:39:49 +0200

    change log level colors

--- src/main/utils/logging/NetworkLogLevels.java
+++ src/main/utils/logging/NetworkLogLevels.java
@@ -1,14 +1,35 @@
 package utils.logging;
 
+import utils.SenderType;
+import utils.events.SocketEventArguments;
+
 import java.util.logging.Level;
 
 public class NetworkLogLevels extends Level {
-    public static final Level CONTROLLER = new NetworkLogLevels("CONTROLLER", 801);
-    public static final Level SWITCH = new NetworkLogLevels("SWITCH", 802);
-    public static final Level REPLICA = new NetworkLogLevels("REPLICA", 803);
-    public static final Level DIFFER = new NetworkLogLevels("DIFFER", 804);
+    public static final NetworkLogLevels CONTROLLER = new NetworkLogLevels("CONTROLLER", 801);
+    public static final NetworkLogLevels SWITCH = new NetworkLogLevels("SWITCH", 802);
+    public static final NetworkLogLevels REPLICA = new NetworkLogLevels("REPLICA", 803);
+    public static final NetworkLogLevels DIFFER = new NetworkLogLevels("DIFFER", 804);
 
     protected NetworkLogLevels(String name, int value) {
         super(name, value);
+    }
+
+    public static Level getLevel(SenderType sender) {
+        if (sender == SenderType.ControllerRegion) {
+            return NetworkLogLevels.CONTROLLER;
+        }
+        if (sender == SenderType.ReplicaRegion) {
+            return NetworkLogLevels.REPLICA;
+        }
+        if (sender == SenderType.SwitchesRegion) {
+            return NetworkLogLevels.SWITCH;
+        }
+        return Level.INFO;
+    }
+
+    public static Level getLevel(SocketEventArguments arg) {
+        SenderType sender = arg.getSenderType();
+        return getLevel(sender);
     }
 }
