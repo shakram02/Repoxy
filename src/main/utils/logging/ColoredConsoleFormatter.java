d53624e465f9b3edb86ead4f883729f8f563b646
shakram02 <ahmedhamdyau@gmail.com>
2017-08-02 08:39:29 +0200

    output formatting

--- src/main/utils/logging/ColoredConsoleFormatter.java
+++ src/main/utils/logging/ColoredConsoleFormatter.java
@@ -9,20 +9,25 @@ public class ColoredConsoleFormatter extends Formatter {
 
     @Override
     public String format(LogRecord logRecord) {
+        String levelColor = this.levelColor(logRecord.getLevel());
 
-        return this.levelColor(logRecord.getLevel()) + "XX" + logRecord.getMessage() + " " + logRecord.getSourceMethodName()
-                + " \n" + ConsoleColors.RESET;
+        return levelColor + "[" + logRecord.getLevel() + " - " +
+                logRecord.getSourceMethodName() + "] "
+                + logRecord.getMessage()
+                + "\n" + ConsoleColors.RESET;
     }
 
     private String levelColor(Level level) {
         if (level == NetworkLogLevels.CONTROLLER) {
-            return ConsoleColors.YELLOW_BOLD;
+            return ConsoleColors.BLUE_BOLD;
         } else if (level == NetworkLogLevels.REPLICA) {
             return ConsoleColors.GREEN;
         } else if (level == NetworkLogLevels.SWITCH) {
-            return ConsoleColors.CYAN_BOLD;
+            return ConsoleColors.CYAN;
         } else if (level == NetworkLogLevels.DIFFER) {
             return ConsoleColors.RED_UNDERLINED;
+        } else if (level == Level.INFO) {
+            return ConsoleColors.BLUE_BRIGHT;
         }
         return ConsoleColors.WHITE;
     }
