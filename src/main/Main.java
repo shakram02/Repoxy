2fbf82f3aec22642ecd894143e52807a4cf03a3c
shakram02 <ahmedhamdyau@gmail.com>
2017-08-15 15:31:27 +0200

    add config variables for multiple VMs

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,27 +9,23 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.*;
 
-/**
- * Packet flow:
- * **Input**
- * - Sockets
- * - Regions
- * - Mediators
- * - Regions
- * - Sockets
- * **Output**
- * <p>
- * The behaviour of the program is non-determined with respect to a disconnected
- * replicated controller, no actions will be taken to resolve that.
+/*
+  The Guest OSs connect to
+
+  vboxnet0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
+  inet 192.168.56.1  netmask 255.255.255.0  broadcast 192.168.56.255
  */
 public class Main {
-    public static final String LOCALHOST = "127.0.0.1";
+    public static final String CLONE_SEC_CONT = "192.168.56.105";
+    public static final String CLONE_MAIN_CONT = "192.168.56.104";
+    public static final String LOCALHOST = "192.168.56.1";
+
     public static int OF_PORT = 6833;
     public static int CONTROLLER_PORT = 6834;
     public static int REPLICATED_CONTROLLER_PORT = 6835;
 
     public static final int WIND_SIZE = 20;
-    public static final int TIMESTAMP_THRESHOLD_MS = 100;
+    public static final int TIMESTAMP_THRESHOLD_MS = 200;
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) throws IOException {
@@ -39,8 +35,8 @@ public class Main {
                 OF_PORT, CONTROLLER_PORT, REPLICATED_CONTROLLER_PORT));
 
         ProxyBuilder builder = ProxyBuilder.createInstance()
-                .addController(LOCALHOST, CONTROLLER_PORT)
-                .addController(LOCALHOST, REPLICATED_CONTROLLER_PORT);
+                .addController(CLONE_MAIN_CONT, CONTROLLER_PORT)
+                .addController(CLONE_SEC_CONT, REPLICATED_CONTROLLER_PORT);
 
         builder.startServer(LOCALHOST, OF_PORT);
         final ProxyMediator mediator = builder.getMediator();
