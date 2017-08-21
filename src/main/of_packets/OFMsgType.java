3c60e41658f518f389c4288c62d9317bcea0c33a
shakram02 <ahmedhamdyau@gmail.com>
2017-08-21 10:27:08 +0200

    Functioning OFMsgType
    Create reply/query sets
    Create message query/reply mapping

--- src/main/of_packets/OFMsgType.java
+++ src/main/of_packets/OFMsgType.java
@@ -1,7 +1,46 @@
 package of_packets;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.LinkedHashSet;
+
 @SuppressWarnings("ALL")
 public class OFMsgType {
+    private static final LinkedHashSet<Byte> QUERY_SET;
+    private static final LinkedHashSet<Byte> REPLY_SET;
+    private static final BiMap<Byte, Byte> MESSAGE_PAIRS;
+
+    public static boolean isQuery(Byte msgCode) {
+        return QUERY_SET.contains(msgCode);
+    }
+
+    public static boolean isReply(Byte msgCode) {
+        return REPLY_SET.contains(msgCode);
+    }
+
+    /**
+     * Calculates the opposite of a given Request-Reply packet
+     *
+     * @param msgCode code of the request/reply
+     * @return Code of the opposite message
+     */
+    @NotNull
+    public static Byte getOppositeMessage(Byte msgCode) {
+        if (!QUERY_SET.contains(msgCode) && !REPLY_SET.contains(msgCode)) {
+            throw new IllegalStateException(String.format("Not a request, nor a reply [%s]", msgCode));
+        }
+
+        // Perform forward and backward search of message codes
+        Byte forwardSearchResult = MESSAGE_PAIRS.get(msgCode);
+
+        if (forwardSearchResult == null) {
+            return MESSAGE_PAIRS.inverse().get(msgCode);
+        }
+
+        return forwardSearchResult;
+    }
 
     // Symmetric/Immutable messages;
     public static final Byte OFPT_HELLO = 0;
@@ -43,4 +82,47 @@ public class OFMsgType {
     // Controller/Switch message
     public static final Byte OFPT_QUEUE_GET_CONFIG_REQUEST = 20;
     public static final Byte OFPT_QUEUE_GET_CONFIG_REPLY = 21;
+
+    // TODO this should be loaded from protocol def. somehow
+    // TODO use reflication to add to set? idk.
+    static {
+        int pairCount = 6;
+
+        QUERY_SET = new LinkedHashSet<>(pairCount);
+
+        QUERY_SET.add(OFMsgType.OFPT_BARRIER_REQUEST);
+        QUERY_SET.add(OFMsgType.OFPT_ECHO_REQUEST);
+        QUERY_SET.add(OFMsgType.OFPT_FEATURES_REQUEST);
+        QUERY_SET.add(OFMsgType.OFPT_GET_CONFIG_REQUEST);
+        QUERY_SET.add(OFMsgType.OFPT_QUEUE_GET_CONFIG_REQUEST);
+        QUERY_SET.add(OFMsgType.OFPT_STATS_REQUEST);
+
+
+        REPLY_SET = new LinkedHashSet<>(pairCount);
+
+        REPLY_SET.add(OFMsgType.OFPT_BARRIER_REPLY);
+        REPLY_SET.add(OFMsgType.OFPT_ECHO_REPLY);
+        REPLY_SET.add(OFMsgType.OFPT_FEATURES_REPLY);
+        REPLY_SET.add(OFMsgType.OFPT_GET_CONFIG_REPLY);
+        REPLY_SET.add(OFMsgType.OFPT_QUEUE_GET_CONFIG_REPLY);
+        REPLY_SET.add(OFMsgType.OFPT_STATS_REQUEST);
+
+        MESSAGE_PAIRS = HashBiMap.create();
+        convertSetsToBiMap(QUERY_SET, REPLY_SET);
+    }
+
+    private static void convertSetsToBiMap(LinkedHashSet requestSet, LinkedHashSet replySet) {
+        // Note that LinkedHashSet returns element in insertion order
+
+        Byte[] replySetArray = new Byte[replySet.size()];
+        REPLY_SET.toArray(replySetArray);
+
+        Byte[] requestSetArray = new Byte[requestSet.size()];
+        QUERY_SET.toArray(requestSetArray);
+
+        for (int i = 0; i < replySetArray.length; i++) {
+            MESSAGE_PAIRS.put(requestSetArray[i], replySetArray[i]);
+        }
+        int x = 3;
+    }
 }
