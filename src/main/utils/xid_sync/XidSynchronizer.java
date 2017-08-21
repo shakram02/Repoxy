02157af54c253a894cc2a23fd7a3df6a09739fc1
shakram02 <ahmedhamdyau@gmail.com>
2017-08-21 10:52:25 +0200

    modify xid synchronizer

--- src/main/utils/xid_sync/XidSynchronizer.java
+++ src/main/utils/xid_sync/XidSynchronizer.java
@@ -1,82 +1,119 @@
 package utils.xid_sync;
 
+import com.google.common.collect.ImmutableList;
 import of_packets.OFMsgType;
 import of_packets.OFPacket;
 import of_packets.OFPacketHeader;
+import org.jetbrains.annotations.NotNull;
 import utils.ConnectionId;
 import utils.events.SocketDataEventArg;
+import utils.packet_store.PacketStore;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Optional;
 
 /**
- * Controllers might send different Xid numbers. This class fixes this issue
+ * Controllers might send different OFXid numbers. This class fixes this issue
  * <p>
  * FIXME
  * I assume that messages are sent back from switches in order. Hence, I use
  * a queue to store Xids. If this is not true, then a REQUEST - REPLY table should
- * be made and Xid are fetched/stored according to message type.
+ * be made and OFXid are fetched/stored according to message type.
  */
 public class XidSynchronizer {
-    private RequestBuffer requestBuffer;
-    private static final Set<Byte> REQUEST_SET;
-    private static final Set<Byte> REPLY_SET;
+    PacketStore packetStore;
 
-    public XidSynchronizer() {
-        requestBuffer = new RequestBuffer();
+    public XidSynchronizer(PacketStore packetStore) {
+        this.packetStore = packetStore;
     }
 
     /**
      * Adjust the message, it might be a request/reply
      * some requests are initiated by both network sides
-     * that's why we just call it adjust as we don't yet know
-     *
-     * @param arg Socket argument containing OpenFlow packets
+     * that's why we just call it syncListXids as we don't yet know
      */
-    public void adjust(SocketDataEventArg arg) {
-        for (OFPacket packet : arg.getPackets()) {
-            this.adjustPacket(arg.getId(), packet);
+    public ImmutableList<OFPacket> syncListXids(@NotNull final ConnectionId id, @NotNull final ImmutableList<OFPacket> packets) {
+        final ImmutableList.Builder<OFPacket> listBuilder = ImmutableList.builder();
+
+        for (OFPacket packet : packets) {
+            Optional<OFPacket> syncResult = syncPacketXid(id, packet);
+
+            if (syncResult.isPresent()) {
+                listBuilder.add(syncResult.get());
+            } else {
+                listBuilder.add(packet);
+            }
         }
+
+        return listBuilder.build();
     }
 
     /**
-     * Adjust Xid of request / reply packets
+     * Adjust OFXid of request / reply packets
      *
      * @param packet OpenFlow packet
      */
-    private void adjustPacket(ConnectionId id, OFPacket packet) {
-        Byte messageCode = packet.getHeader().getMessageCode();
+    @NotNull
+    private Optional<OFPacket> syncPacketXid(ConnectionId id, OFPacket packet) {
+        OFPacketHeader header = packet.getHeader();
+        Byte msgCode = header.getMessageCode();
 
-        if (REQUEST_SET.contains(messageCode)) {
-
-            this.requestBuffer.addRequest(id, packet.getHeader());
-
-        } else if (REPLY_SET.contains(messageCode)) {
-
-            Integer adjustedXid = this.requestBuffer.getRequest(id, packet.getHeader());
-            packet.getHeader().setXid(adjustedXid);
+        if (OFMsgType.isReply(msgCode)) {
+            return Optional.of(modifyReply(id, packet));
         }
+
+        if (OFMsgType.isQuery(msgCode)) {
+            storePacket(id, packet);
+        }
+
+        return Optional.empty();
     }
 
-    // TODO this should be loaded from protocol def. somehow
-    static {
-        REQUEST_SET = new HashSet<>();
+    private OFPacket modifyReply(ConnectionId id, OFPacket reply) {
+        Byte messageCode = reply.getHeader().getMessageCode();
 
-        REQUEST_SET.add(OFMsgType.OFPT_BARRIER_REQUEST);
-        REQUEST_SET.add(OFMsgType.OFPT_ECHO_REQUEST);
-        REQUEST_SET.add(OFMsgType.OFPT_FEATURES_REQUEST);
-        REQUEST_SET.add(OFMsgType.OFPT_GET_CONFIG_REQUEST);
-        REQUEST_SET.add(OFMsgType.OFPT_QUEUE_GET_CONFIG_REQUEST);
-        REQUEST_SET.add(OFMsgType.OFPT_STATS_REQUEST);
+        if (!hasCounterpart(id, messageCode)) {
+            throw new IllegalStateException("No query is present for this reply");
+        }
 
+        OFPacket query = retrievePacket(id, OFMsgType.getOppositeMessage(messageCode));
 
-        REPLY_SET = new HashSet<>();
-
-        REPLY_SET.add(OFMsgType.OFPT_BARRIER_REPLY);
-        REPLY_SET.add(OFMsgType.OFPT_ECHO_REPLY);
-        REPLY_SET.add(OFMsgType.OFPT_FEATURES_REPLY);
-        REPLY_SET.add(OFMsgType.OFPT_GET_CONFIG_REPLY);
-        REPLY_SET.add(OFMsgType.OFPT_QUEUE_GET_CONFIG_REPLY);
-        REPLY_SET.add(OFMsgType.OFPT_STATS_REQUEST);
+        return this.setPacketXid(reply, query.getHeader().getXId());
     }
+
+    private Optional<OFPacket> handleQuery(ConnectionId id, OFPacket query) {
+        Byte messageCode = query.getHeader().getMessageCode();
+
+        if (hasCounterpart(id, messageCode)) {
+            throw new IllegalStateException("Reply is present before its query");
+        }
+
+        storePacket(id, query);
+        return Optional.empty();
+    }
+
+    private boolean hasCounterpart(ConnectionId id, Byte messageCode) {
+        return packetStore.exists(id, OFMsgType.getOppositeMessage(messageCode));
+    }
+
+    private void storePacket(ConnectionId id, OFPacket packet) {
+        this.packetStore.storePacket(id, packet);
+    }
+
+    private OFPacket retrievePacket(ConnectionId id, Byte messageCode) {
+        return this.packetStore.getPacket(id, messageCode);
+    }
+
+
+    /**
+     * Creates a copy of the packet with a new xid
+     *
+     * @param packet packet to change its xid
+     * @param xid    new xid
+     * @return returns a copy of the packet with the new xid
+     */
+    @NotNull
+    private OFPacket setPacketXid(OFPacket packet, int xid) {
+        return OFPacket.createNewWithXid(packet, xid);
+    }
+
 }
