6041ecdb6ccebcbb873da06314b68ecbdbe7785e
shakram02 <ahmedhamdyau@gmail.com>
2017-08-26 11:55:50 +0200

    Finish SynchronizationFacade
    - Provide minimal interface to external classes
    - Manage I/O queue of parent buffers
    - Abstract I/O queues to just functions

--- src/main/network_io/io_synchronizer/SynchronizationFacade.java
+++ src/main/network_io/io_synchronizer/SynchronizationFacade.java
@@ -1,26 +1,34 @@
 package network_io.io_synchronizer;
 
+import utils.SenderType;
 import utils.events.SocketDataEventArg;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 /**
  * Provides an interface for synchronization mechanism and hides implementation details
  */
-public class SynchronizationFacade implements Synchronizer {
+public class SynchronizationFacade {
     private XidSynchronizer xidSynchronizer;
     private ClonedControllerPacketSynchronizer delaySynchronizer;
 
-    public SynchronizationFacade() {
+    private Consumer<SocketDataEventArg> sendToController;
+    private Consumer<SocketDataEventArg> sendToSwitches;
+
+    public SynchronizationFacade(Consumer<SocketDataEventArg> sendToSwitches,
+                                 Consumer<SocketDataEventArg> sendToController) {
         this.xidSynchronizer = new XidSynchronizer();
         this.delaySynchronizer = new ClonedControllerPacketSynchronizer();
+        this.sendToSwitches = sendToSwitches;
+        this.sendToController = sendToController;
     }
 
-    public void addUnSynchronized(SocketDataEventArg arg) {
+    private void addUnSynchronized(SocketDataEventArg arg) {
         this.delaySynchronizer.addUnSynchronized(arg);
     }
 
-    public Optional<SocketDataEventArg> getSynced() {
+    private Optional<SocketDataEventArg> getSynced() {
         // Get delay synchronized packet
         Optional<SocketDataEventArg> delaySynced = this.delaySynchronizer.getSynced();
 
@@ -37,6 +45,34 @@ public class SynchronizationFacade implements Synchronizer {
         // Modify Xid if needed
         Optional<SocketDataEventArg> fullySynced = this.xidSynchronizer.syncIfReply(timeSyncedEventArg);
         return Optional.of(fullySynced.orElse(timeSyncedEventArg));
+    }
 
+    private void updateIoQueues() {
+        Optional<SocketDataEventArg> synced = this.getSynced();
+
+        while (synced.isPresent()) {
+            SocketDataEventArg dataEventArg = synced.get();
+            if (dataEventArg.getSenderType() == SenderType.SwitchesRegion) {
+                this.sendToController.accept(dataEventArg);
+            } else {
+                // We disregard the type of the controller as it might change over time
+                this.sendToSwitches.accept(dataEventArg);
+            }
+
+            synced = this.getSynced();
+        }
+    }
+
+    public void manageInput(SocketDataEventArg incoming) {
+        this.manageIo(incoming);
+    }
+
+    public void manageOutput(SocketDataEventArg incoming) {
+        this.manageIo(incoming);
+    }
+
+    private void manageIo(SocketDataEventArg incoming) {
+        this.addUnSynchronized(incoming);
+        this.updateIoQueues();
     }
 }
