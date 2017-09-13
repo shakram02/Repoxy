e146d7d81b70dd0e6f8a2a548c958bd2546ffb91
shakram02 <ahmedhamdyau@gmail.com>
2017-09-13 08:37:07 +0200

    support start/stop operations

--- user-interface/src/Controller.kt
+++ user-interface/src/Controller.kt
@@ -4,17 +4,44 @@ class Controller {
 
     private lateinit var backgroundThread: Thread
     private val builder: ProxyBuilder = ProxyBuilder.createInstance()
-
+    private var isRunning = false
 
     fun startServer(configurator: Configurator) {
-        // Add other controllers
-        configurator.getConfigs().forEach({ addController(it.first, it.second) })
+        if (isRunning) return
 
-        backgroundThread = Thread({ builder.startServer(configurator.localIp, configurator.localPort) })
+        backgroundThread = Thread({ threadMain(configurator) })
+
         backgroundThread.start()
+        isRunning = true
     }
 
     private fun addController(ip: String, port: Int) {
         builder.addController(ip, port)
+    }
+
+    fun stopServer() {
+        if (!isRunning) return
+
+        backgroundThread.interrupt()
+        isRunning = false
+    }
+
+    private fun threadMain(configurator: Configurator) {
+
+        val portList = ArrayList<String>()
+
+
+        // Add other controllers
+        configurator.getConfigs().forEach({
+            addController(it.ip, it.port)
+            portList.add(it.port.toString())
+        })
+
+        val portStringBuilder = StringBuilder()
+        portList.joinTo(portStringBuilder)
+
+        builder.startServer(configurator.localIp, configurator.localPort)
+        println(String.format("Local IP: [%s] Ports: [%d] [%s]",
+                configurator.localIp, configurator.localPort, portStringBuilder.toString()))
     }
 }
