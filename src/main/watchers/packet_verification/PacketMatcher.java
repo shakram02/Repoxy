8ef321810ad38915e4cf47fdf5f05353cf3e2028
shakram02 <ahmedhamdyau@gmail.com>
2017-09-18 05:08:59 +0200

    stateless packet matching

--- src/main/watchers/packet_verification/PacketMatcher.java
+++ src/main/watchers/packet_verification/PacketMatcher.java
@@ -7,28 +7,11 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 class PacketMatcher {
-    /**
-     * Stops the iterator at the matched packet
-     *
-     * @param stampedPacket           packet from main controller
-     * @param secondaryPacketIterator Iterator for the packets of the secondary controller
-     * @return true if a secondary packet matches the main packet
-     */
-    public boolean findByIterator(final StampedPacket stampedPacket, Iterator<StampedPacket> secondaryPacketIterator) {
-        OFPacket mainPacket = stampedPacket.getPacket();
+    public boolean match(final StampedPacket first, StampedPacket second) {
+        OFPacket firstPacket = first.getPacket();
+        OFPacket secondPacket = second.getPacket();
 
-        while (secondaryPacketIterator.hasNext()) {
-            OFPacket secondary = secondaryPacketIterator.next().getPacket();
-
-            if (mainPacket.getMessageCode() != secondary.getMessageCode()) {
-                continue;
-            }
-
-            if (!Arrays.equals(mainPacket.getData(), secondary.getData())) {
-                throw new IllegalStateException("Data from two controllers don't match");
-            }
-            return true;
-        }
-        return false;
+        return firstPacket.getMessageCode() == secondPacket.getMessageCode() &&
+                Arrays.equals(firstPacket.getData(), secondPacket.getData());
     }
 }
