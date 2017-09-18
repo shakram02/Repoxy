72c00f7e18596a36f66b4d169d6eb134ab29dd11
shakram02 <ahmedhamdyau@gmail.com>
2017-09-18 12:35:56 +0200

    allow wild 4sec delay for one time

--- src/main/watchers/packet_verification/TimeoutChecker.java
+++ src/main/watchers/packet_verification/TimeoutChecker.java
@@ -4,14 +4,22 @@ import utils.StampedPacket;
 
 class TimeoutChecker {
     private final long threshold;
+    private boolean isNew;
+    private static final int ARBITRARY_UNKNOWN_DELAY = 4000;    // TODO: some random 5sec delay happens once, idk
 
     public TimeoutChecker(final long threshold) {
         this.threshold = threshold;
     }
 
     public boolean hasTimedOut(final StampedPacket packet, final StampedPacket secondary) {
+
         long delay = Math.abs(packet.getTimestamp() - secondary.getTimestamp());
         System.out.println("Delay:" + delay);
+
+        if (isNew && (delay > ARBITRARY_UNKNOWN_DELAY)) {
+            isNew = false;
+            return false;
+        }
 
         return delay > threshold;
     }
