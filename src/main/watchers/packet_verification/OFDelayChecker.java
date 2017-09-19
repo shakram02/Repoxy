f924238667a817fcd0ce841f29a5e224faa64f9c
shakram02 <ahmedhamdyau@gmail.com>
2017-09-19 09:01:56 +0200

    last packet timeout

--- src/main/watchers/packet_verification/OFDelayChecker.java
+++ src/main/watchers/packet_verification/OFDelayChecker.java
@@ -63,6 +63,12 @@ public class OFDelayChecker implements SocketEventObserver {
             return;
         }
 
+        if (sender == SenderType.ControllerRegion &&
+                this.timeoutChecker.isMainPacketTimedOut(arg.getTimestamp())) {
+            logger.warning("Early detected timeout main controller packet");
+            switchController();
+        }
+
         // Loop on all elements to validate previously unvalidated packets
         this.updateState();
 
--- src/main/watchers/packet_verification/TimeoutChecker.java
+++ src/main/watchers/packet_verification/TimeoutChecker.java
@@ -1,14 +1,24 @@
 package watchers.packet_verification;
 
+import of_packets.OFPacket;
 import utils.StampedPacket;
 
 class TimeoutChecker {
     private final long threshold;
     private boolean isNew;
     private static final int ARBITRARY_UNKNOWN_DELAY = 4000;    // TODO: some random 5sec delay happens once, idk
+    private long lastMainPacketTimestamp;
 
     public TimeoutChecker(final long threshold) {
         this.threshold = threshold;
+        this.lastMainPacketTimestamp = System.currentTimeMillis();
+    }
+
+    public boolean isMainPacketTimedOut(final long mainPacketTimestamp) {
+        boolean timedOut = Math.abs(lastMainPacketTimestamp - mainPacketTimestamp) > threshold;
+        lastMainPacketTimestamp = mainPacketTimestamp;
+
+        return timedOut;
     }
 
     public boolean hasTimedOut(final StampedPacket packet, final StampedPacket secondary) {
