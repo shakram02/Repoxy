3f6c34afb553a8beda048a004dad23a1e6916929
shakram02 <ahmedhamdyau@gmail.com>
2017-09-19 12:01:05 +0200

    use dumper in timeout checking

--- src/main/watchers/packet_verification/TimeoutChecker.java
+++ src/main/watchers/packet_verification/TimeoutChecker.java
@@ -1,6 +1,6 @@
 package watchers.packet_verification;
 
-import of_packets.OFPacket;
+import utils.Dumper;
 import utils.StampedPacket;
 
 class TimeoutChecker {
@@ -8,10 +8,13 @@ class TimeoutChecker {
     private boolean isNew;
     private static final int ARBITRARY_UNKNOWN_DELAY = 4000;    // TODO: some random 5sec delay happens once, idk
     private long lastMainPacketTimestamp;
+    private final String FILE_NAME = "DELAYS.txt";
+    private Dumper<String> dumper;
 
     public TimeoutChecker(final long threshold) {
         this.threshold = threshold;
         this.lastMainPacketTimestamp = System.currentTimeMillis();
+        this.dumper = new Dumper<>(String::getBytes);
     }
 
     public boolean isMainPacketTimedOut(final long mainPacketTimestamp) {
@@ -24,7 +27,10 @@ class TimeoutChecker {
     public boolean hasTimedOut(final StampedPacket packet, final StampedPacket secondary) {
 
         long delay = Math.abs(packet.getTimestamp() - secondary.getTimestamp());
-        System.out.println("Delay:" + delay);
+        String info = "Delay:" + delay;
+
+        System.out.println(info);
+        this.dumper.dump(info, FILE_NAME);
 
         if (isNew && (delay > ARBITRARY_UNKNOWN_DELAY)) {
             isNew = false;
