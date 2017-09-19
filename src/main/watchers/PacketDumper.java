02be903fcf891108b3df42425d21d31252d5ee72
shakram02 <ahmedhamdyau@gmail.com>
2017-09-19 12:00:46 +0200

    isolate packet dumper

--- src/main/watchers/PacketDumper.java
+++ src/main/watchers/PacketDumper.java
@@ -3,48 +3,36 @@ package watchers;
 import of_packets.OFPacket;
 import of_packets.OFStreamParser;
 import org.jetbrains.annotations.NotNull;
+import utils.Dumper;
 import utils.SenderType;
 import utils.events.SocketDataEventArg;
 import utils.events.SocketEventArguments;
 import utils.events.SocketEventObserver;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 
 public class PacketDumper implements SocketEventObserver {
 
+    private final Dumper<OFPacket> fileDumper;
     String fileName;
     HashMap<SenderType, Path> pathCache;
 
     public PacketDumper(String fileName) throws IOException {
-        Path logDirectory = getLogDirectoryPath();
-
-        if (!Files.exists(logDirectory)) {
-            // TODO: monitor file sizes, delete oldest
-            Files.createDirectory(logDirectory);
-        }
-
-        // TODO: flush the written files every now and then
-        this.fileName = logDirectory.toString() + "/" + fileName;
+        this.fileName = fileName;
+        fileDumper = new Dumper<>(this::serializeToBytes);
         pathCache = new HashMap<>();
     }
 
-    private void dumpPacket(OFPacket packet, SenderType type) {
+    private byte[] serializeToBytes(OFPacket packet) {
+        return OFStreamParser.serializePacket(packet).array();
+    }
 
+    private void dumpPacket(SocketDataEventArg arg, SenderType type) {
         Path path = getPathFromCache(type);
-
-        try {
-            Files.write(path, OFStreamParser.serializePacket(packet).array(),
-                    StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new IllegalArgumentException("Couldn't write to file");
-        }
+        this.fileDumper.dump(arg.getPacket(), path);
     }
 
     @Override
@@ -52,12 +40,7 @@ public class PacketDumper implements SocketEventObserver {
         if (!(eventArgs instanceof SocketDataEventArg)) return;
 
         SocketDataEventArg dataEventArg = (SocketDataEventArg) eventArgs;
-        dumpPacket(dataEventArg.getPacket(), dataEventArg.getSenderType());
-    }
-
-    private static Path getLogDirectoryPath() {
-        // Current folder /log
-        return Paths.get(Paths.get("").toAbsolutePath() + "/log");
+        dumpPacket(dataEventArg, dataEventArg.getSenderType());
     }
 
     private Path getPathFromCache(SenderType type) {
