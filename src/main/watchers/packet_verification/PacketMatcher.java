1b4527caa98601f78b82922fb4efb0c234b19010
shakram02 <ahmedhamdyau@gmail.com>
2017-09-21 05:41:17 +0200

    prepare for comparing using packet buffers

--- src/main/watchers/packet_verification/PacketMatcher.java
+++ src/main/watchers/packet_verification/PacketMatcher.java
@@ -1,11 +1,29 @@
 package watchers.packet_verification;
 
 import of_packets.OFPacket;
+import utils.PacketBuffer;
+import utils.SenderType;
 import utils.events.SocketDataEventArg;
 
 import java.util.Arrays;
 
 class PacketMatcher {
+    private final PacketBuffer mainControllerPackets;
+    private final PacketBuffer secondaryControllerPackets;
+
+    PacketMatcher() {
+        mainControllerPackets = new PacketBuffer();
+        secondaryControllerPackets = new PacketBuffer();
+    }
+
+    public void addPacket(final SocketDataEventArg packet) {
+        if (packet.getSenderType() == SenderType.ControllerRegion) {
+            this.mainControllerPackets.addPacket(packet.getId(), packet);
+        } else {
+            this.secondaryControllerPackets.addPacket(packet.getId(), packet);
+        }
+    }
+
     public boolean match(final SocketDataEventArg first, SocketDataEventArg second) {
         OFPacket firstPacket = first.getPacket();
         OFPacket secondPacket = second.getPacket();
