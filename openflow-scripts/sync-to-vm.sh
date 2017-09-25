a7f410b0a16dd3ee56e6535cd291320632a222e0
shakram02 <ahmedhamdyau@gmail.com>
2017-09-25 12:07:08 +0200

    overwrite old files

--- openflow-scripts/sync-to-vm.sh
+++ openflow-scripts/sync-to-vm.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
-#! /usr/bin/expect
-
-pass="mininet"
 
 if [ "$#" -ne 1 ];then
  echo "Invalid number of arguments"
  exit 1
 fi
 
-expect "mn0@192.168.1.241's password: "
-send "$pass"
-rsync -rav ssh \
+pass="mininet"
+ip="192.168.1.2$1"
+
+rsync -ravI ssh \
 /mnt/Exec/code/java/of-Dwoxy/openflow-scripts/controller-component/l2_all_to_controller.py \
-"mn0@192.168.1.2$1:/home/mn0/proxy/ext/"
+"mn0@$ip:/home/mn0/pox/ext/"
 
-
-rsync -rav ssh \
-/mnt/Exec/code/java/of-Dwoxy/openflow-scripts/mininet-topology/*.py \
-"mn0@192.168.1.2$1:/home/mn0/px/mininet-topology/"
-expect "mn0@192.168.1.2$1's password: "
-send "$pass"
+rsync -ravI ssh \
+/mnt/Exec/code/java/of-Dwoxy/openflow-scripts/mininet_topology/ \
+"mn0@$ip:/home/mn0/px/mininet_topology/"
