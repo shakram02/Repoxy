1c5b12eb1cefd3c571858ef1f77d8db957c228b6
shakram02 <ahmedhamdyau@gmail.com>
2017-11-24 14:52:52 +0200

    edit docs, add error queue

--- src/main/middleware/ProxyMiddleware.java
+++ src/main/middleware/ProxyMiddleware.java
@@ -7,17 +7,20 @@ import java.util.concurrent.LinkedTransferQueue;
 
 /**
  * Splits packet processing pipeline into manageable operations
- * Each middleware class acts upon a single connection ID. this
+ * <p>
+ * Each middleware class acts upon a SINGLE {@link utils.ConnectionId}. this
  * makes the data management in a central location (before the start
  * of the pipeline) and simplifies the logic inside the middleware
  */
 public abstract class ProxyMiddleware {
     protected LinkedTransferQueue<SocketDataEventArg> input;
     protected LinkedTransferQueue<SocketDataEventArg> output;
+    protected LinkedTransferQueue<SocketDataEventArg> error;
 
     public ProxyMiddleware() {
         input = new LinkedTransferQueue<>();
         output = new LinkedTransferQueue<>();
+        error = new LinkedTransferQueue<>();
     }
 
     public final void addInput(SocketDataEventArg input) {
@@ -31,7 +34,16 @@ public abstract class ProxyMiddleware {
         return this.output.poll();
     }
 
+    @NotNull
+    public final SocketDataEventArg getError() {
+        return this.error.poll();
+    }
+
     public boolean isEmpty() {
         return this.output.isEmpty();
+    }
+
+    public boolean isError() {
+        return !this.error.isEmpty();
     }
 }
