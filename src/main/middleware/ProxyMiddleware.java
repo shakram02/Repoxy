d2cefb78523972a3134a14834cdfb6ca0b8d5e4d
shakram02 <ahmedhamdyau@gmail.com>
2017-11-24 14:31:31 +0200

    use LinkedQueue to avoid adding comparator

--- src/main/middleware/ProxyMiddleware.java
+++ src/main/middleware/ProxyMiddleware.java
@@ -3,7 +3,7 @@ package middleware;
 import org.jetbrains.annotations.NotNull;
 import utils.events.SocketDataEventArg;
 
-import java.util.PriorityQueue;
+import java.util.concurrent.LinkedTransferQueue;
 
 /**
  * Splits packet processing pipeline into manageable operations
@@ -12,12 +12,12 @@ import java.util.PriorityQueue;
  * of the pipeline) and simplifies the logic inside the middleware
  */
 public abstract class ProxyMiddleware {
-    protected PriorityQueue<SocketDataEventArg> input;
-    protected PriorityQueue<SocketDataEventArg> output;
+    protected LinkedTransferQueue<SocketDataEventArg> input;
+    protected LinkedTransferQueue<SocketDataEventArg> output;
 
     public ProxyMiddleware() {
-        input = new PriorityQueue<>();
-        output = new PriorityQueue<>();
+        input = new LinkedTransferQueue<>();
+        output = new LinkedTransferQueue<>();
     }
 
     public final void addInput(SocketDataEventArg input) {
@@ -31,7 +31,7 @@ public abstract class ProxyMiddleware {
         return this.output.poll();
     }
 
-    public boolean hasOutput() {
-        return !this.output.isEmpty();
+    public boolean isEmpty() {
+        return this.output.isEmpty();
     }
 }
