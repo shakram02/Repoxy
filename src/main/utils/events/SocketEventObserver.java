0843ec0aa32e171dca19c02a99809625ae5da34d
shakram02 <ahmedhamdyau@gmail.com>
2017-11-24 14:39:23 +0200

    add docs

--- src/main/utils/events/SocketEventObserver.java
+++ src/main/utils/events/SocketEventObserver.java
@@ -3,6 +3,9 @@ package utils.events;
 import com.google.common.eventbus.Subscribe;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * An interface representing a non-blocking middleware
+ */
 public interface SocketEventObserver {
     @Subscribe
     void dispatchEvent(@NotNull SocketEventArguments eventArgs);
