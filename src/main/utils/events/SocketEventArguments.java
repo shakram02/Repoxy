22af6ea9d08ed046f0e3ce9fbf67c83003db12cb
shakram02 <ahmedhamdyau@gmail.com>
2017-11-25 11:38:35 +0200

    use milliseconds for timestamps, don't use nano seconds

--- src/main/utils/events/SocketEventArguments.java
+++ src/main/utils/events/SocketEventArguments.java
@@ -11,7 +11,7 @@ public abstract class SocketEventArguments {
 
     @Value.Lazy
     public long getTimestamp() {
-        return System.nanoTime();
+        return System.currentTimeMillis();
     }
 
     public abstract EventType getReplyType();
