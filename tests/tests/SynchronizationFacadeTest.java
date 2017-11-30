a17dc7e6a06ef4d4be99cbbf25b6b4d6c627ff8d
shakram02 <ahmedhamdyau@gmail.com>
2017-11-30 11:24:24 +0200

    update tests

--- tests/tests/SynchronizationFacadeTest.java
+++ tests/tests/SynchronizationFacadeTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import middleware.blocking.io_synchronizer.SynchronizationFacade;
 import of_packets.OFMsgType;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,129 +9,141 @@ import utils.SenderType;
 public class SynchronizationFacadeTest {
     @Test
     public void simpleOnePacket() {
-        FacadeTestHelper tester = new FacadeTestHelper();
+        SynchronizationFacade f = new SynchronizationFacade();
         int requestXid = 54;
-
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(1,
+        f.addInput(TestPacketArgMaker.createFromPacket(1,
                 TestPackets.BarrierRequestXid54, SenderType.ReplicaRegion));
-        tester.facade.execute();
 
-        Assert.assertTrue(tester.checkPacket(1, SenderType.ReplicaRegion, requestXid,
+        f.execute();
+
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(1, f.getOutput(), requestXid,
                 OFMsgType.OFPT_BARRIER_REQUEST));
 
-        tester.addUnSynchronized(TestPacketArgMaker
-                .createFromPacket(1,
-                        TestPackets.BarrierReplyXid54, SenderType.SwitchesRegion));
+        f.addInput(TestPacketArgMaker
+                .createFromPacket(1, TestPackets.BarrierReplyXid54, SenderType.SwitchesRegion));
 
-        Assert.assertTrue(tester.checkPacket(1, SenderType.SwitchesRegion,
+        f.execute();
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(1, f.getOutput(),
                 requestXid, OFMsgType.OFPT_BARRIER_REPLY));
 
-        Assert.assertTrue(tester.absence());   // Assert that nothing is left
+        Assert.assertFalse(f.hasOutput());
     }
 
     @Test
     public void simpleOnePacketReversedDifferentXids() {
-        FacadeTestHelper tester = new FacadeTestHelper();
-
+        SynchronizationFacade f = new SynchronizationFacade();
         int requestXid = 11;
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(1,
+        f.addInput(TestPacketArgMaker.createFromPacket(1,
                 TestPackets.FeaturesReplyXid10, SenderType.SwitchesRegion));
 
-        Assert.assertTrue(tester.absence());
+        // Don't forget to execute before assertion
+        f.execute();
+        Assert.assertFalse(f.hasOutput());
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(1,
+        f.addInput(TestPacketArgMaker.createFromPacket(1,
                 TestPackets.FeaturesRequestXid11, SenderType.ReplicaRegion));
 
         // Request and reply come out with the xid of the request
-        Assert.assertTrue(tester.checkPacket(1, SenderType.ReplicaRegion,
+        f.execute();
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(1, f.getOutput(),
                 requestXid, OFMsgType.OFPT_FEATURES_REQUEST));
 
-        Assert.assertTrue(tester.checkPacket(1, SenderType.SwitchesRegion,
+        f.execute();
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(1, f.getOutput(),
                 requestXid, OFMsgType.OFPT_FEATURES_REPLY));
 
-        Assert.assertTrue(tester.absence());   // Assert that nothing is left
+        f.execute();
+        Assert.assertFalse(f.hasOutput());   // Assert that nothing is left
     }
 
     @Test
     public void simpleOnePacketReversedSameXids() {
-        FacadeTestHelper tester = new FacadeTestHelper();
+        SynchronizationFacade f = new SynchronizationFacade();
         int requestXid = 10;
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(1,
+        f.addInput(TestPacketArgMaker.createFromPacket(1,
                 TestPackets.FeaturesReplyXid10, SenderType.SwitchesRegion));
 
-        Assert.assertTrue(tester.absence());
+        f.execute();
+        Assert.assertFalse(f.hasOutput());
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(1,
+        f.addInput(TestPacketArgMaker.createFromPacket(1,
                 TestPackets.FeaturesRequestXid10, SenderType.ReplicaRegion));
 
         // Request and reply come out with the xid of the request
-        Assert.assertTrue(tester.checkPacket(1, SenderType.ReplicaRegion,
+        f.execute();
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(1, f.getOutput(),
                 requestXid, OFMsgType.OFPT_FEATURES_REQUEST));
 
-        Assert.assertTrue(tester.checkPacket(1, SenderType.SwitchesRegion,
+        f.execute();
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(1, f.getOutput(),
                 requestXid, OFMsgType.OFPT_FEATURES_REPLY));
-        Assert.assertTrue(tester.absence());   // Assert that nothing is left
+
+        f.execute();
+        Assert.assertFalse(f.hasOutput());   // Assert that nothing is left
     }
 
     @Test
     public void twoPacketsSameXidsDifferentConnections() {
-        FacadeTestHelper tester = new FacadeTestHelper();
+        SynchronizationFacade f = new SynchronizationFacade();
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(1,
+        f.addInput(TestPacketArgMaker.createFromPacket(1,
                 TestPackets.FeaturesReplyXid10, SenderType.SwitchesRegion));
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(2,
+        f.addInput(TestPacketArgMaker.createFromPacket(2,
                 TestPackets.BarrierRequestXid54, SenderType.ReplicaRegion));
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(2,
+        f.addInput(TestPacketArgMaker.createFromPacket(2,
                 TestPackets.BarrierReplyXid54, SenderType.SwitchesRegion));
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(1,
+        f.addInput(TestPacketArgMaker.createFromPacket(1,
                 TestPackets.FeaturesRequestXid10, SenderType.ReplicaRegion));
 
-        Assert.assertTrue(tester.checkPacket(2, SenderType.ReplicaRegion, 54,
+        f.execute();
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(2, f.getOutput(), 54,
                 OFMsgType.OFPT_BARRIER_REQUEST));
 
-        Assert.assertTrue(tester.checkPacket(2, SenderType.SwitchesRegion, 54,
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(2, f.getOutput(), 54,
                 OFMsgType.OFPT_BARRIER_REPLY));
 
-        Assert.assertTrue(tester.checkPacket(1, SenderType.ReplicaRegion, 10,
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(1, f.getOutput(), 10,
                 OFMsgType.OFPT_FEATURES_REQUEST));
 
-        Assert.assertTrue(tester.checkPacket(1, SenderType.SwitchesRegion, 10,
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(1, f.getOutput(), 10,
                 OFMsgType.OFPT_FEATURES_REPLY));
     }
 
     @Test
     public void twoPacketsDifferentXidsDifferentConnections() {
-        FacadeTestHelper tester = new FacadeTestHelper();
+        SynchronizationFacade f = new SynchronizationFacade();
         int barrierRequestXid = 53;
         int barrierReplyXid = 11;
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(1,
+        f.addInput(TestPacketArgMaker.createFromPacket(1,
                 TestPackets.FeaturesReplyXid10, SenderType.SwitchesRegion));
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(2,
+        f.addInput(TestPacketArgMaker.createFromPacket(2,
                 TestPackets.BarrierRequestXid53, SenderType.ReplicaRegion));
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(2,
+        f.addInput(TestPacketArgMaker.createFromPacket(2,
                 TestPackets.BarrierReplyXid54, SenderType.SwitchesRegion));
 
-        tester.addUnSynchronized(TestPacketArgMaker.createFromPacket(1,
+        f.addInput(TestPacketArgMaker.createFromPacket(1,
                 TestPackets.FeaturesRequestXid11, SenderType.ReplicaRegion));
 
-        Assert.assertTrue(tester.checkPacket(2, SenderType.ReplicaRegion, barrierRequestXid,
+        f.execute();
+
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(2, f.getOutput(), barrierRequestXid,
                 OFMsgType.OFPT_BARRIER_REQUEST));
 
-        Assert.assertTrue(tester.checkPacket(2, SenderType.SwitchesRegion, barrierRequestXid,
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(2, f.getOutput(), barrierRequestXid,
                 OFMsgType.OFPT_BARRIER_REPLY));
 
-        Assert.assertTrue(tester.checkPacket(1, SenderType.ReplicaRegion, barrierReplyXid,
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(1, f.getOutput(), barrierReplyXid,
                 OFMsgType.OFPT_FEATURES_REQUEST));
 
-        Assert.assertTrue(tester.checkPacket(1, SenderType.SwitchesRegion, barrierReplyXid,
+        Assert.assertTrue(AssertionHelper.hasValidIdMessageTypeXid(1, f.getOutput(), barrierReplyXid,
                 OFMsgType.OFPT_FEATURES_REPLY));
     }
 }
